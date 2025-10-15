1a71de887e08d6d66bfecce9fe22d2496c4eeb14
Derek Castillo <castilloderekandre@gmail.com>
2025-10-14 20:30:13 -0500

    GUI

--- quickpandas.py
+++ quickpandas.py
@@ -4,120 +4,252 @@ pd.set_option('future.no_silent_downcasting', True)
 import re
 import math
 import numpy as np
+import customtkinter as ctk
+
 from pathlib import Path
+from tkinter import filedialog
+from CTkMessagebox import CTkMessagebox
 
-schema = [ 'Mileage' ]
-retail_inventory = pd.DataFrame(
-        {
-            'LAST 6 OF VIN': [], 
-            'YEAR': [], 
-            'MAKE': [], 
-            'MODEL': [], 
-            'MILEAGE': [], 
-            'LOCATION': [], 
-            'INVENTORY ($)': [], 
-            'EXPENSES ($)': [], 
-            'TOTAL INVESTED ($)': [], 
-            'MISC.': [], 
-            'SOURCED FROM': [], 
-            'SRP ($)': [], 
-            'DATE RECEIVED': [], 
-            'OPEN INVOICE?': [], 
-            'AGE': []
-        })
+class FileHandler:
+    def __init__(self):
+        self.paths = {}
 
-def parse_description(description):
-    schema_elements = []
-    for identifier in schema:
-        index = description.index(identifier) + len(identifier) + 1
+    def select_file(self, key, label):
+        path = filedialog.askopenfilename(
+            title = 'Select Excel File',
+            filetypes=[('Excel files', '*.xls'), ('Excel files', '*.xlsx')]
+        )
 
-        try:
-            end_index = description.index('\n', index)
-        except ValueError:
-            end_index = len(description)
+        if not path:
+            return
+
+        self.paths[key] = Path(path)
+        label.configure(text=self.paths[key].name)
+
+    def select_directory(self, key):
+        path = filedialog.asksaveasfile(
+            title = 'Save As',
+            filetypes=[('Excel files', '*.xlsx')]
+        )
+
+        if not path:
+            return
+
+        self.paths[key] = path
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.app_title = 'Reportwise'
+        self.title(self.app_title)
+
+        self.count = 0
+
+        self.PRODUCTS_KEY = 'products'
+        self.EXPENSES_KEY = 'expenses'
+        self.INVENTORY_KEY = 'inventory'
+        self.OUTPUT_KEY = 'output'
+
+        self.filehandler = FileHandler()
+
+        self.label_1 = ctk.CTkLabel(self, text='Products/Services Excel File')
+        self.label_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky='ew')
+
+        self.label_2 = ctk.CTkLabel(self, text='Expenses Excel File')
+        self.label_2.grid(row=0, column=1, padx=10, pady=(10, 0), sticky='ew')
+
+        self.label_3 = ctk.CTkLabel(self, text='Previous Retail Inventory Excel File')
+        self.label_3.grid(row=0, column=2, padx=10, pady=(10, 0), sticky='ew')
+
+        #
+        self.button_select_products_file = ctk.CTkButton(self, text='Select File', command=lambda: self.filehandler.select_file(self.PRODUCTS_KEY, self.label_products_basename))
+        self.button_select_products_file.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
         
-        schema_elements.append(description[index:end_index])
+        self.label_products_basename = ctk.CTkLabel(self, text='No File Selected')
+        self.label_products_basename.grid(row=2, column=0, padx=10, pady=10, sticky='ew')
 
-    return schema_elements
+        #
+        self.button_select_expenses_file = ctk.CTkButton(self, text='Select File', command=lambda: self.filehandler.select_file(self.EXPENSES_KEY, self.label_expenses_basename))
+        self.button_select_expenses_file.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
 
-def parse_product_name(name):
-    breakdown = []
-    pattern = r":(\d+)\s+(\w+)\s+(.+)\(VIN#.+\)"
-    capture_groups = re.search(pattern, name)
-    if capture_groups:
-        breakdown.extend(list(capture_groups.groups()))
-    else:
-        breakdown = ['-', '-', '-']
+        self.label_expenses_basename = ctk.CTkLabel(self, text='No File Selected')
+        self.label_expenses_basename.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
+
+        #
+        self.button_select_inventory_file = ctk.CTkButton(self, text='Select File', command=lambda: self.filehandler.select_file(self.INVENTORY_KEY, self.label_inventory_basename))
+        self.button_select_inventory_file.grid(row=1, column=2, padx=10, pady=10, sticky='ew')
+
+        self.label_inventory_basename = ctk.CTkLabel(self, text='No File Selected')
+        self.label_inventory_basename.grid(row=2, column=2, padx=10, pady=10, sticky='ew')
+
+        #
+        self.button_save_as = ctk.CTkButton(self, text='Save As', command=self.save_as)
+        self.button_save_as.grid(row=3, column=0, padx=10, pady=10, columnspan=3, sticky='ew')
+
+        #
+        self.schema = [ 'Mileage' ]
+        self.retail_inventory = pd.DataFrame(
+            {
+                'LAST 6 OF VIN': [], 
+                'YEAR': [], 
+                'MAKE': [], 
+                'MODEL': [], 
+                'MILEAGE': [], 
+                'LOCATION': [], 
+                'INVENTORY ($)': [], 
+                'EXPENSES ($)': [], 
+                'TOTAL INVESTED ($)': [], 
+                'MISC.': [], 
+                'SOURCED FROM': [], 
+                'SRP ($)': [], 
+                'DATE RECEIVED': [], 
+                'OPEN INVOICE?': [], 
+                'AGE': []
+            })
+
+        self.update_idletasks()
+
+    def parse_description(self, description):
+        schema_elements = []
+        for identifier in self.schema:
+            description = str(description)
+            index = description.find(identifier) 
+
+            if (index == -1):
+                schema_elements.append('Incorrect Schema')
+                continue
+
+            index += len(identifier) + 1
+
+            end_index = description.find('\n', index)
+
+            if (end_index == -1):
+                end_index = len(description)
+            
+            schema_elements.append(description[index:end_index])
+
+        return schema_elements
+
+    def parse_product_name(self, name):
+        breakdown = []
+        pattern = r":(\d+)\s+(\w+)\s+(.+)\(VIN#.+\)"
+        capture_groups = re.search(pattern, name)
+        if capture_groups:
+            breakdown.extend(list(capture_groups.groups()))
+        else:
+            breakdown = ['-', '-', '-']
+            
+        return breakdown
+
+    def parse_product(self, index, product, expenses):
+        vin = product.SKU[-6:]
+        inventory = product['Purchase Cost']
+        expenses = sum(expenses.loc[vin])
+        product_series = [
+            vin, # LAST 6 OF VIN
+            *self.parse_product_name(product['Product/Service Name']), # YEAR, MAKE, MODEL
+            *self.parse_description(product['Sales Description']), # MILEAGE (BASED ON SCHEMA)
+            None, # LOCATION
+            inventory, # INVENTORY
+            expenses, # EXPENSES
+            inventory + expenses, # TOTAL INVESTED
+            None, # MISC
+            None, # SOURCED FROM
+            product['Sales Price / Rate'], # SRP
+            None, # DATE RECEIVED
+            None, # OPEN INVOICE
+            f'=TODAY() - N{index+2}', # AGE
+        ]
+
+        return product_series
+
+    def create_retail_report(self, products, expenses):
+        for index, product in products.iterrows():
+            parsed_product = self.parse_product(index, product, expenses)
+            self.retail_inventory.loc[len(self.retail_inventory)] = parsed_product
+
+    def update_retail_inventory(self, old_retail_inventory):
+        self.retail_inventory['LOCATION'] = old_retail_inventory['LOCATION']
+        self.retail_inventory['MISC.'] = old_retail_inventory['MISC.']
+        self.retail_inventory['SOURCED FROM'] = old_retail_inventory['SOURCED FROM']
+        self.retail_inventory['DATE RECEIVED'] = old_retail_inventory['DATE RECEIVED']
+        self.retail_inventory['OPEN INVOICE?'] = old_retail_inventory['OPEN INVOICE?']
+
+    def show_popup(self, report):
+        CTkMessagebox(title="Info", message="A file has not been selected. Cannot create report!") 
+
+    def main(self):
+        self.retail_inventory = pd.DataFrame(
+            {
+                'LAST 6 OF VIN': [], 
+                'YEAR': [], 
+                'MAKE': [], 
+                'MODEL': [], 
+                'MILEAGE': [], 
+                'LOCATION': [], 
+                'INVENTORY ($)': [], 
+                'EXPENSES ($)': [], 
+                'TOTAL INVESTED ($)': [], 
+                'MISC.': [], 
+                'SOURCED FROM': [], 
+                'SRP ($)': [], 
+                'DATE RECEIVED': [], 
+                'OPEN INVOICE?': [], 
+                'AGE': []
+            })
+
+
+        expenses = pd.read_excel(self.filehandler.paths[self.EXPENSES_KEY])
+        products_and_services = pd.read_excel(self.filehandler.paths[self.PRODUCTS_KEY])
+
+        products = products_and_services.loc[products_and_services.Type == 'Inventory', ['Product/Service Name', 'Sales Description', 'SKU', 'Sales Price / Rate', 'Purchase Cost']]
+
+        expenses = expenses.loc[[3, 9, 10, 11]]
+        expenses = expenses.T
+        expenses.columns = expenses.iloc[0]
+        expenses = expenses[1:-1]
+        expenses.set_index('Distribution account', inplace=True)
+
+        def clean_index(index):
+            return str(index).split('.')[0]
+
+        expenses.index = expenses.index.map(clean_index)
+
+        expenses.fillna(0, inplace=True)
+        expenses = expenses.infer_objects(copy=False)
+
+        self.create_retail_report(products, expenses)
         
-    return breakdown
+        if self.INVENTORY_KEY in self.filehandler.paths:
+            old_retail_inventory = pd.read_excel(self.filehandler.paths[self.INVENTORY_KEY])
+            self.update_retail_inventory(old_retail_inventory)
 
-def parse_product(index, product, expenses):
-    vin = product.SKU[-6:]
-    inventory = product['Purchase Cost']
-    expenses = sum(expenses.loc[vin])
-    product_series = [
-        vin, # LAST 6 OF VIN
-        *parse_product_name(product['Product/Service Name']), # YEAR, MAKE, MODEL
-        *parse_description(product['Sales Description']), # MILEAGE (BASED ON SCHEMA)
-        None, # LOCATION
-        inventory, # INVENTORY
-        expenses, # EXPENSES
-        inventory + expenses, # TOTAL INVESTED
-        None, # MISC
-        None, # SOURCED FROM
-        product['Sales Price / Rate'], # SRP
-        None, # DATE RECEIVED
-        None, # OPEN INVOICE
-        f'=TODAY() - N{index+2}', # AGE
-    ]
+    def save_as(self):
+        if not self.PRODUCTS_KEY in self.filehandler.paths:
+            self.show_popup('Products/Services')
+            return
 
-    return product_series
+        if not self.EXPENSES_KEY in self.filehandler.paths:
+            self.show_popup('Expenses')
+            return
 
-def create_retail_report(products, expenses):
-    global retail_inventory
-    for index, product in products.iterrows():
-        parsed_product = parse_product(index, product, expenses)
-        retail_inventory.loc[len(retail_inventory)] = parsed_product
+        if not self.INVENTORY_KEY in self.filehandler.paths:
+            msg = CTkMessagebox(title='Warning', message='Previous retail inventory file not selected. Manual changes will not carry over to new file', icon='warning', option_1='Continue', option_2='Cancel')
+            response = msg.get()
 
-def update_retail_inventory(old_retail_inventory):
-    global retail_inventory
-    retail_inventory['LOCATION'] = old_retail_inventory['LOCATION']
-    retail_inventory['MISC.'] = old_retail_inventory['MISC.']
-    retail_inventory['SOURCED FROM'] = old_retail_inventory['SOURCED FROM']
-    retail_inventory['DATE RECEIVED'] = old_retail_inventory['DATE RECEIVED']
-    retail_inventory['OPEN INVOICE?'] = old_retail_inventory['OPEN INVOICE?']
+            if response=='Cancel':
+                return
+
+        self.filehandler.select_directory(self.OUTPUT_KEY)
+
+        if not self.OUTPUT_KEY in self.filehandler.paths:
+            return
+
+        self.main()
+        
+        self.retail_inventory.to_excel(self.filehandler.paths[self.OUTPUT_KEY].name)
 
 
-def on_import():
-    global retail_inventory
-
-    expenses = pd.read_excel('./Reports/Katy Truck and Equipment Sales LLC_CURRENT INVENTORY.xlsx')
-    products_and_services = pd.read_excel('./Reports/ProductServiceList.xls')
-
-    products = products_and_services.loc[products_and_services.Type == 'Inventory', ['Product/Service Name', 'Sales Description', 'SKU', 'Sales Price / Rate', 'Purchase Cost']]
-
-    expenses = expenses.loc[[3, 9, 10, 11]]
-    expenses = expenses.T
-    expenses.columns = expenses.iloc[0]
-    expenses = expenses[1:-1]
-    expenses.set_index('Distribution account', inplace=True)
-
-    def clean_index(index):
-        return str(index).split('.')[0]
-
-    expenses.index = expenses.index.map(clean_index)
-
-    expenses.fillna(0, inplace=True)
-    expenses = expenses.infer_objects(copy=False)
-
-    create_retail_report(products, expenses)
-    
-    old_retail_inventory_path = Path('./Reports/retail_inventory.xlsx')
-
-    if old_retail_inventory_path.exists():
-        old_retail_inventory = pd.read_excel(old_retail_inventory_path)
-        update_retail_inventory(old_retail_inventory)
-
-    retail_inventory.to_excel('./Reports/retail_inventory.xlsx')
-
-on_import()
+app = App()
+app.mainloop()
