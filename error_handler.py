745153e759092f0499852e3af0cb3f2bea2156ab
castilloderekandre <castilloderekandre@gmail.com>
2025-12-14 20:18:53 -0600

    Made some changes to error_handler. Implemented error_handler still in testing

--- error_handler.py
+++ error_handler.py
@@ -4,31 +4,35 @@ import datetime
 import traceback
 from CTkMessagebox import CTkMessagebox
 
-# sys.stdout = open(log_path, "w", encoding='utf-8')
-# sys.stderr = sys.stdout
-
-print(f"Logging started at {datetime.datetime.now()}")
-print(f"Log file: {log_path}\n")
-
-LOG_FILE = "error.log"
-
-def log_error_to_file(exc_type, exc_value, exc_traceback):
+def log_error_to_file(type, value, tb):
     os.makedirs("logs", exist_ok=True)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_path = os.path.join("logs", f"reportwise_{timestamp}.log")
 
     with open(log_path, "a", encoding="utf-8") as log_file:
-        log_file.write(f"[{datetime.datetime.now():%Y-%m-%d %H:%M:%S}] {exc_type.__name__}: {exc_value}\n")
-        # traceback.print_exception(exc_type, exc_value, exc_traceback, file=f)
-        traceback.print_tb(exc_traceback, file=log_file)
+        log_file.write(f"[{datetime.datetime.now():%Y-%m-%d %H:%M:%S}] {type.__name__}: {value}\n")
+        # traceback.print_exception(type, value, tb, file=f)
+        traceback.print_tb(tb, file=log_file)
         log_file.write("\n")
 
-def show_exception_popup(exc_type, exc_value, exc_traceback):
+
+def show_exception_popup(type, value, tb):
+    """
+    Shows a popup through CTkMessagebox for an unhandled exception.
+
+    Args:
+        type (ERROR TYPE): exception class.
+        value (ERROR TYPE): exception instance.
+        tb (ERROR TYPE): traceback.
+
+    Returns:
+        nothing
+    """
     # Log it first
-    log_error_to_file(exc_type, exc_value, exc_traceback)
+    log_error_to_file(type, value, tb)
 
     # Then show popup
-    msg = f"{exc_type.__name__}: {exc_value}"
+    msg = f"{type.__name__}: {value}"
     CTkMessagebox(title="Unhandled Exception", message=msg, icon="cancel")
 
 def install_global_handler():
